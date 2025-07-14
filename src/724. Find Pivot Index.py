b2946396e64182b589b409caac9f65d232ebffc5
admin <osintfir@gmail.com>
2025-07-14 16:59:30 +0300

    feat: solve this one
    Time complexity: O(n)
    Space complexity: O(1)

---
+++ src/724. Find Pivot Index.py
@@ -0,0 +1,16 @@
+from typing import List
+
+
+class Solution:
+    def pivotIndex(self, nums: List[int]) -> int:
+        nums_sum = sum(nums)
+        left_sum = 0
+
+        for index, value in enumerate(nums):
+            if left_sum == nums_sum - left_sum - value:
+                return index
+
+            left_sum += value
+
+        return -1
+        
